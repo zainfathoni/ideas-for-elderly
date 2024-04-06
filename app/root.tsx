047fba1500809b6e5a4044564cbee861563366df
Zain Fathoni <me@zainf.dev>
2024-04-06 19:02:24 +0800

    Install Tailwind CSS
    Co-authored-by: Ahmad Dzuizz Annajib <dzuizz@users.noreply.github.com>

--- app/root.tsx
+++ app/root.tsx
@@ -1,3 +1,4 @@
+import { LinksFunction } from "@remix-run/node";
 import {
   Links,
   Meta,
@@ -5,6 +6,9 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import styles from "./tailwind.css";
+
+export const links: LinksFunction = () => [{ rel: "stylesheet", href: styles }];
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
