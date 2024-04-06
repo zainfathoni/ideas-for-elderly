28db2686313d54808e0cf5585480527f77003ce0
Zain Fathoni <me@zainf.dev>
2024-04-06 21:39:24 +0800

    Integratie Tailwind CSS for Remix Vite

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,8 @@
-import type { Config } from "tailwindcss";
-
+/** @type {import('tailwindcss').Config} */
 export default {
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-} satisfies Config;
+};
