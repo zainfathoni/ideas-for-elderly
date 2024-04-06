508f927a1ced0f5a104de19ffe6ba6627408fda0
Zain Fathoni <me@zainf.dev>
2024-04-07 00:47:36 +0800

    Update activity.tsx with parsedContent logging

--- app/routes/activity.tsx
+++ app/routes/activity.tsx
@@ -21,6 +21,12 @@ export default function Activity() {
   const { activity } = useLoaderData<{ activity: Message }>();
   console.log(activity);
 
+  const content = activity.content;
+  console.log(content);
+
+  const parsedContent = JSON.parse(content);
+  console.log(parsedContent);
+
   return (
     <div className="bg-white py-24 sm:py-32">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
